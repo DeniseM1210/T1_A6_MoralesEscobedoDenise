51a30618d7317ddf3b2c876a3a0adc131827bbe5
Denise Morales Escobedo <denisemorales8764@gmail.com>
2020-10-02 11:45:17 -0500

    Se añadieron atributos a la clase Aspirantes, así como constructores, getters y setter y setters

--- src/Actividad6.java
+++ src/Actividad6.java
@@ -1,4 +1,51 @@
+import java.util.Arrays;
 
+class Aspirante{
+	private int folio;
+	private String nombre;
+	private byte edad;
+	private String redesSociales[];
+	public Aspirante(int folio, String nombre, byte edad, String[] redesSociales) {
+		this.folio = folio;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.redesSociales = redesSociales;
+	}
+	public int getFolio() {
+		return folio;
+	}
+	public void setFolio(int folio) {
+		this.folio = folio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public byte getEdad() {
+		return edad;
+	}
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+	public String[] getRedesSociales() {
+		return redesSociales;
+	}
+	public void setRedesSociales(String[] redesSociales) {
+		this.redesSociales = redesSociales;
+	}
+	
+	@Override
+	public String toString() {
+		return "Aspirante [folio=" + folio + ", nombre=" + nombre + ", edad=" + edad + ", redesSociales="
+				+ Arrays.toString(redesSociales) + "]";
+	}
+	
+	
+	
+	
+}
 public class Actividad6 {
 
 	public static void main(String[] args) {
